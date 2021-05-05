ff517f9831df133dd7baa2c300c6e60172b973e0
Tiago Penha Pedroso <tiago.souza.pedroso@gmail.com>
2021-05-05 14:35:54 -0300

    Criei meu primeiro codigo Java

---
+++ primeiroCodigoJava/Mensagem.java
@@ -0,0 +1,7 @@
+public class Mensagem {
+    public static void main(String[] args){
+        //System.out.println("Olá!");
+        System.out.println("Olá " + args[0] + " " + args[1] + "! :)");
+        System.out.println("Tchau!!!");
+    }
+}
